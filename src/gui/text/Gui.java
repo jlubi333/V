a4588086741afb03d5926d669441653ab986fb59
Justin Lubin <jlubi333@gmail.com>
2014-08-15 18:54:16 -0400

    Extracted code from Main into Gui.

---
+++ src/gui/text/Gui.java
@@ -0,0 +1,63 @@
+package gui.text;
+
+import core.GameMap;
+import core.entity.Entity;
+import core.tile.TextBasedTile;
+import util.GameInfo;
+import util.MapParser;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Gui {
+    public Gui(String level, String fontName) throws FileNotFoundException, ClassNotFoundException {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(Color.BLACK);
+
+        JPanel sidebar = new JPanel();
+        sidebar.setLayout(new GridBagLayout());
+        sidebar.setPreferredSize(new Dimension(300, 500));
+
+        GridBagConstraints logConstraints = new GridBagConstraints();
+        JTextArea log = new JTextArea();
+        log.setEditable(false);
+        log.setBackground(new Color(255, 255, 255, 100));
+        log.setBorder(new EmptyBorder(10, 10, 10, 10));
+        log.setText("Welcome to " + GameInfo.GAME_NAME + ".");
+        logConstraints.gridx = 0;
+        logConstraints.gridy = 0;
+        logConstraints.weightx = 1.0;
+        logConstraints.weighty = 1.0;
+        logConstraints.fill = GridBagConstraints.BOTH;
+        sidebar.add(log, logConstraints);
+
+        GridBagConstraints textInputConstraints = new GridBagConstraints();
+        JTextField textInput = new JTextField();
+        textInput.setBackground(new Color(255, 255, 255, 100));
+        textInputConstraints.gridx = 0;
+        textInputConstraints.gridy = 1;
+        textInputConstraints.weightx = 1.0;
+        textInputConstraints.fill = GridBagConstraints.HORIZONTAL;
+        sidebar.add(textInput, textInputConstraints);
+
+        mainPanel.add(sidebar);
+
+        java.util.List<java.util.List<TextBasedTile>> tiles = MapParser.parseFile(level);
+        GameMap<TextBasedTile> map = new GameMap<>(tiles, new ArrayList<Entity>());
+        Font f = new Font(fontName, Font.PLAIN, 16);
+        GamePanel gamePanel = new GamePanel(map, f);
+        gamePanel.setPreferredSize(new Dimension(500, 500));
+
+        mainPanel.add(gamePanel);
+
+        JFrame frame = new JFrame(GameInfo.GAME_NAME);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.add(mainPanel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
--- src/gui/text/Main.java
+++ src/gui/text/Main.java
@@ -1,64 +1,13 @@
 package gui.text;
 
-import core.GameMap;
-import core.entity.Entity;
-import core.tile.TextBasedTile;
-import util.GameInfo;
-import util.MapParser;
-
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException {
-        JPanel mainPanel = new JPanel();
-        mainPanel.setBackground(Color.BLACK);
-
-        JPanel sidebar = new JPanel();
-        sidebar.setLayout(new GridBagLayout());
-        sidebar.setPreferredSize(new Dimension(300, 500));
-
-        GridBagConstraints logConstraints = new GridBagConstraints();
-        JTextArea log = new JTextArea();
-        log.setEditable(false);
-        log.setBackground(new Color(255, 255, 255, 100));
-        log.setBorder(new EmptyBorder(10, 10, 10, 10));
-        log.setText("Welcome to " + GameInfo.GAME_NAME + ".");
-        logConstraints.gridx = 0;
-        logConstraints.gridy = 0;
-        logConstraints.weightx = 1.0;
-        logConstraints.weighty = 1.0;
-        logConstraints.fill = GridBagConstraints.BOTH;
-        sidebar.add(log, logConstraints);
-
-        GridBagConstraints textInputConstraints = new GridBagConstraints();
-        JTextField textInput = new JTextField();
-        textInput.setBackground(new Color(255, 255, 255, 100));
-        textInputConstraints.gridx = 0;
-        textInputConstraints.gridy = 1;
-        textInputConstraints.weightx = 1.0;
-        textInputConstraints.fill = GridBagConstraints.HORIZONTAL;
-        sidebar.add(textInput, textInputConstraints);
-
-        mainPanel.add(sidebar);
-
-        List<List<TextBasedTile>> tiles = MapParser.parseFile("map.jplm");
-        GameMap<TextBasedTile> map = new GameMap<>(tiles, new ArrayList<Entity>());
-        Font f = new Font("Courier New", Font.PLAIN, 16);
-        GamePanel gamePanel = new GamePanel(map, f);
-        gamePanel.setPreferredSize(new Dimension(500, 500));
-
-        mainPanel.add(gamePanel);
-
-        JFrame frame = new JFrame(GameInfo.GAME_NAME);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.add(mainPanel);
-        frame.pack();
-        frame.setVisible(true);
+    public static void main(String[] args) {
+        try {
+            Gui gui = new Gui("Courier New", "map.jplm");
+        } catch (FileNotFoundException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
