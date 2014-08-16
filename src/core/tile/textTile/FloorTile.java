b8a193732a2c652314350cb58dd71fe4dbbf088b
Justin Lubin <jlubi333@gmail.com>
2014-08-16 03:10:29 -0400

    Improved floor tile appearance.

--- src/core/tile/textTile/FloorTile.java
+++ src/core/tile/textTile/FloorTile.java
@@ -7,6 +7,6 @@ import java.awt.*;
 
 public class FloorTile extends Tile {
     public FloorTile() {
-        super("Floor", false, new TileDisplay(' ', new Color(0, 0, 0, 0), new Color(0, 0, 0)));
+        super("Floor", false, new TileDisplay('.', new Color(255, 255, 255, 50), new Color(0, 0, 0, 0)));
     }
 }
